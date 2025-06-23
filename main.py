f114512eea5c06852ce6a705bb2c30d5969b007f
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-06-23 15:39:44 +0800

    non working code

---
+++ main.py
@@ -0,0 +1,3 @@
+"""Main file."""
+
+import model
---
+++ model/__init__.py
@@ -0,0 +1,5 @@
+"""This module initializes the model package and imports necessary submodules."""
+
+from .configuration_heliumbert import *
+from .heliumbert_global import *
+from .modelling_heliumbert import *
