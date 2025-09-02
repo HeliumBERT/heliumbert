b7aefcc610575490353a14c07353112f277694e8
TNTzx <azelynnrituapersonality@gmail.com>
2025-09-02 17:25:11 +0800

    remove arabic characters

--- src/heliumbert/tokenization_heliumbert.py
+++ src/heliumbert/tokenization_heliumbert.py
@@ -2,8 +2,13 @@
 """Tokenization classes for HeliumBERT model."""
 
 from transformers import AlbertTokenizer
+import re
 
 class HeliumbertTokenizer(AlbertTokenizer):
     """Represents a HeliumBERT tokenizer."""
+    def _tokenize(self, text: str, **kwargs) -> list[str]:
+        text = re.sub(r'[\u0600-\u06FF]+', '', text)
+
+        return super()._tokenize(text, **kwargs)
 
 __all__ = ["HeliumbertTokenizer"]
